307c7ebd5af2f25aca9055234590a3677eb4cb27
Kostas Xanthopoulos <konxantho@gmail.com>
2020-12-07 14:03:45 +0200

    typecasting method

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 def search(iteratable, field, value):
-    """Finds the item in the iterable that has item.fiedl == value"""
+    """Returns the item in the iterable for which item.field == value"""
     try:
         return next(item for item in iteratable if getattr(item, field) == value)
     except StopIteration:
         return
+
+
+def typecasted_value(property):
+    """Typecasting property.value to property.type
+    Args:
+        property (Property Model): Contains default, name and type attributes
+    """
+    type_map = {'Float': float, 'Integer': int, 'Boolean': bool}
+    typecast_func = type_map[property.type]
+    if property.value:
+        return typecast_func(property.value)
