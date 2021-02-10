dc85fd6a92df5c1761a930592cac452f465dab5c
Kostas Xanthopoulos <konxantho@gmail.com>
2021-02-10 13:17:37 +0200

    split example to pub-sub and rpc

---
+++ examples/ps_example.py
@@ -0,0 +1,42 @@
+import sys
+
+from nodem.parser import NodesHandler
+from nodem.definitions import MODELS_DIR_PATH
+
+
+def get_example_node_parser():
+    example_model_path = f'{MODELS_DIR_PATH}/example.ent'
+    return NodesHandler(example_model_path)
+
+
+def get_example_publisher(node_parser):
+    return node_parser.nodes[0].publishers[0]
+
+
+def get_example_subscriber(node_parser):
+    return node_parser.nodes[-1].subscribers[0].commlib_subscriber
+
+
+def _is_service_arg_valid(service_arg):
+    valid_arguments = ['s', 'sub', 'subscriber', 'p', 'pub', 'publisher']
+    return service_arg in valid_arguments
+
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if arguments:
+        service_arg = arguments[0]
+        if not _is_service_arg_valid(service_arg):
+            print('[ERROR] Invalid service argument...')
+            exit()
+    else:
+        print('[ERROR] Please add the service argument...')
+        exit()
+
+    node_parser = get_example_node_parser()
+
+    if service_arg in ['s', 'sub', 'subscriber']:
+        example_subscriber = get_example_subscriber(node_parser)
+        example_subscriber.run_forever()
+    elif service_arg in ['p', 'pub', 'publisher']:
+        example_publisher = get_example_publisher(node_parser)
---
+++ examples/rpc_example copy.py
@@ -0,0 +1,43 @@
+import sys
+
+from nodem.definitions import MODELS_DIR_PATH
+from nodem.parser import NodesHandler, AddTwoIntMessage
+
+
+def get_example_node_parser():
+    example_model_path = f'{MODELS_DIR_PATH}/example.ent'
+    return NodesHandler(example_model_path)
+
+
+def get_example_rpc_service(node_parser):
+    return node_parser.nodes[0].rpc_services[0].commlib_rpc_service
+
+
+def get_example_rpc_client(node_parser):
+    return node_parser.nodes[-1].rpc_clients[0].commlib_rpc_client
+
+
+def _is_service_arg_valid(service_arg):
+    valid_arguments = ['c', 'client', 's', 'service']
+    return service_arg in valid_arguments
+
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if arguments:
+        service_arg = arguments[0]
+        if not _is_service_arg_valid(service_arg):
+            print('[ERROR] Invalid service argument...')
+            exit()
+    else:
+        print('[ERROR] Please add the service argument...')
+        exit()
+
+    node_parser = get_example_node_parser()
+
+    if service_arg in ['c', 'client']:
+        example_rpc_client = get_example_rpc_client(node_parser)
+        msg = AddTwoIntMessage.Request(a=1, b=2)
+    elif service_arg in ['sv', 'service']:
+        example_rpc_service = get_example_rpc_service(node_parser)
+        example_rpc_service.run_forever()
