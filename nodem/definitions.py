584e2613449a8a386706b7ccf9808b0edd63915c
Kostas Xanthopoulos <konxantho@gmail.com>
2021-02-10 13:17:21 +0200

    🐛 Fix path

--- nodem/definitions.py
+++ nodem/definitions.py
@@ -2,4 +2,4 @@ from os.path import dirname
 
 ROOT_PATH = dirname(__file__)
 MODELS_DIR_PATH = ROOT_PATH + "/models"
-GRAMMAR_PATH = MODELS_DIR_PATH + '/grammar.py'
+GRAMMAR_PATH = MODELS_DIR_PATH + '/grammar.tx'
